5fda0692038bda7c9a03c5e7e0357a5e1a80a048
naveen13684 <37493627+naveen13684@users.noreply.github.com>
2018-09-30 23:07:07 +0530

    Update Create_boot_vagrantbox.sh

--- Create_boot_vagrantbox.sh
+++ Create_boot_vagrantbox.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 ##################################################
 #Script to create and boot a Vagrant Box : Ubuntu
-#Author : Naveen Kumar HS
-#Version : 1.0
 ##################################################
 
 ####VARIABLES###
@@ -27,9 +25,9 @@ check_command_sccess()
 {
         if [ $? -eq 0 ]
         then
-                echo "$(timestamp) : $1"
+                echo "$TIMESTAMP : $1"
         else
-                echo "$(timestamp) : $2 Check /tmp/install.log for errors"
+                echo "$TIMESTAMP : $2 Check /tmp/install.log for errors"
                 exit
         fi
 }
@@ -39,7 +37,7 @@ install_package()
 if [ "$#" -eq 3 ]
 then
 #        apt-get update >>$VAGRANT_LOG 2>&1
-        echo "$(timestamp) : $1 Installation Started."
+        echo "$TIMESTAMP : $1 Installation Started."
         apt-get install $1 -y >>$VAGRANT_LOG 2>&1
         check_command_sccess "$2" "$3"
 else
@@ -56,27 +54,26 @@ do
 done
 
 # Download Vagrant Box
-        echo "$(timestamp) : Download $VAGRANT_BOX_NAME Vagrant box image."
-        $VAGRANT_CMD box add $VAGRANT_BOX_IMAGE $VAGRANT_BOX_IMAGE_URL >>$VAGRANT_LOG 2>&1
-        check_command_sccess "Vagrant box $VAGRANT_BOX_NAME Successfully added." "Vagrant box $VAGRANT_BOX_NAME failed."
-        echo "$(timestamp) : $VAGRANT_BOX_NAME Vagrant box image Downloaded."
-
-#Creating VM
-        echo "$(timestamp) : Creating working directory for Vagrant box $VAGRANT_WORKING_DIR."
+        echo "$TIMESTAMP : Creating working directory for Vagrant box $VAGRANT_BOX_NAME:$VAGRANT_WORKING_DIR"
         mkdir -p $VAGRANT_WORKING_DIR
         cd $VAGRANT_WORKING_DIR
-        echo "$(timestamp) : Creating/Initialising VM - Vagrantfile"
+        echo "$TIMESTAMP : Install/Add $VAGRANT_BOX_NAME Vagrant box."
+        $VAGRANT_CMD box add $VAGRANT_BOX_IMAGE $VAGRANT_BOX_IMAGE_URL >>$VAGRANT_LOG 2>&1
+        check_command_sccess "Vagrant box $VAGRANT_BOX_NAME Successfully added." "Vagrant box $VAGRANT_BOX_NAME failed."
+
+#Creating Vagrant file
+
         $VAGRANT_CMD init $VAGRANT_BOX_NAME >>$VAGRANT_LOG 2>&1
 
 #Brining Vagrant box up
-        echo "$(timestamp) :Starting VM by running vagrant up"
+        echo "$TIMESTAMP :Create Environment by running vagrant up"
         $VAGRANT_CMD up >>$VAGRANT_LOG 2>&1
         check_command_sccess "Vagrant box $VAGRANT_BOX_NAME is up and running" "Vagrant box $VAGRANT_BOX_NAME having issues with booting"
-        echo "$(timestamp) : Status of the Vagrant box $VAGRANT_BOX_NAME"
+        echo "$TIMESTAMP : Status of the Vagrant box $VAGRANT_BOX_NAME"
         $VAGRANT_CMD status
 
 #Check the ssh configuation for Vagrant box
-        echo "$(timestamp) : ssh configuration of Vagrant box $VAGRANT_BOX_NAME"
+        echo "$TIMESTAMP : ssh configuration of Vagrant box $VAGRANT_BOX_NAME"
         $VAGRANT_CMD ssh-config
 
-        echo "$(timestamp) : check $VAGRANT_LOG for more information about installation"
+        echo "$TIMESTAMP : check $VAGRANT_LOG for more information about installation"
